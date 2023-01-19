b4cd40d1bea876c5c0fc4e16a8a2ceb66903f8e6
Rob Sizeland <robcakes69@gmail.com>
2023-01-19 22:55:34 +0000

    Add collision for npc and ceiling

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,8 +6,8 @@ public class Player : MonoBehaviour
 {
 
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -33,8 +33,21 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        //Make it move
-        transform.Translate(moveDelta * Time.deltaTime);
+        //Checks to see if we can move in the directon by casting a box there first. If the box returns null, we're free to move. 
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Make it move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Make it move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
 
 
     }
